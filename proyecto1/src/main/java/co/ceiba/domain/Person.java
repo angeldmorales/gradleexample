371b233363d6ad2b0481d3bfbe64ce152d8f12ef
Angel David Morales Botett - Ceiba Software <angel.morales@ceiba.com.co>
2017-08-14 15:50:22 -0500

    First Commit

---
+++ proyecto1/src/main/java/co/ceiba/domain/Person.java
@@ -0,0 +1,28 @@
+package co.ceiba.domain;
+
+public class Person {
+	
+	private String name;
+	private String lastName;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Person(String name, String lastName) {
+		super();
+		this.name = name;
+		this.lastName = lastName;
+	}
+	
+	
+
+}
